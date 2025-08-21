f10fdc1f9ddeb9b5cadc13f0e557fa3aea4e889e
CHINTHOJU SHIVAKUMAR <shivakumarchinthoju@gmail.com>
2025-08-21 12:46:42 +0530

    env variables

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
+import urllib.parse
 from flask import Flask
 from extensions import db
 from flask_sqlalchemy import SQLAlchemy
 from Routes.booking_route import booking_bp
 from Routes.review_route import review_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # ✅ This loads variables from .env into environment
+
 
 app = Flask(__name__)
+# ✅ Read env variables
+host = os.getenv("HOST_NAME")
+port = os.getenv("PORT")
+user = os.getenv("NAME")
+password = os.getenv("PASSWORD")
 
-
+print(f"{user}:{password}@{host}:{port}")
 # PostgreSQL config
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost:Pinky@143@localhost:5432/Booking Service'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Pinky%40143@localhost:5432/Booking Service'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}:{port}/Booking Service'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
